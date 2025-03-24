126be771fcdb09162ac53e7ea2965f304d5e1096
DHRUB DAS <dhrub27das@gmail.com>
2025-03-25 01:18:43 +0530

    wrote some backend code

---
+++ server/controllers/authControllers/index.js
@@ -0,0 +1,26 @@
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+
+const registerUser = async (req, res) => {
+    const { userName, userEmail, password, role } = req.body;
+    const existingUser = await User.findOne({
+        $or: [{ userEmail }, { userName }]
+    });
+    if (existingUser) {
+        return res.status(400).json({
+            success: false,
+            message: "UserName or UserEmail already Exists."
+        })
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ userName, userEmail, password: hashedPassword });
+
+    await newUser.save();
+
+    return res.status(201).json({
+        success: true,
+        message: "New User Created/Registered SuccessFully"
+    })
+}
+
+module.exports = { registerUser }
---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+    },
+    userEmail: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema);
