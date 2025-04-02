f0b9d9d70a5a89a0a5c332f395d973e5ff43e76c
DHRUB DAS <dhrub27das@gmail.com>
2025-04-02 21:53:12 +0530

    Curriculum Section for the Instructor View Done

--- client/src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
@@ -1,10 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { courseCurriculumInitialFormData } from "@/config";
 import { InstructorContext } from "@/context/instructor-context";
 import React, { useContext } from "react";
 
 function CourseCurriculum() {
     const { courseCurriculumFormData, setCourseCurriculumFormData } = useContext(InstructorContext);
+
+    function handleNewLecture() {
+        setCourseCurriculumFormData([
+            ...courseCurriculumFormData,
+            {
+                ...courseCurriculumInitialFormData[0]
+            }
+        ])
+        console.log(courseCurriculumFormData);
+    }
+
     return (
         <>
             <Card>
@@ -12,9 +27,36 @@ function CourseCurriculum() {
                     <CardTitle>Create Course Curriculum</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <Button>Add Lecture</Button>
+                    <Button onClick={handleNewLecture}>Add Lecture</Button>
                     <div className="mt-4 space-y-4">
-
+                        {
+                            courseCurriculumFormData.map((curriculumItem, index) => (
+                                <div key={index} className="border p-5 ">
+                                    <div className="flex gap-5 items-center">
+                                        <h3 className="font-semibold">Lecture {index + 1}</h3>
+                                        <Input
+                                            name={`title-${index + 1}`}
+                                            placeholder="Enter Lecture Title"
+                                            className="max-w-96"
+                                        />
+                                        <div className="flex items-center space-x-2">
+                                            <Switch
+                                                checked={false}
+                                                id={`freePreview-${index + 1}`}
+                                            />
+                                            <Label htmlFor={`freePreview-${index + 1}`}>Free Preview</Label>
+                                        </div>
+                                    </div>
+                                    <div className="mt-6">
+                                        <Input
+                                            type="file"
+                                            accept="video/*"
+                                            className="mb-4"
+                                        />
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 </CardContent>
             </Card>
